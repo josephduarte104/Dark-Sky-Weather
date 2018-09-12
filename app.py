6370d9797ec35d1649d6ba945fe0d7eae227e3be
Kang Wang <kwang.inbox@gmail.com>
2018-09-12 16:45:31 -0700

    Implement routing endpoints for weather data for Vancouver

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-'''
+"""
 Python-Flask API to fetch, parse, and visualize weather data from the Dark Sky API (forecast.io)
-'''
+"""
 import darksky_api_request as api
 import darksky_api_methods as dsky
 
@@ -11,71 +11,88 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 # > Redirected endpoints
-@app.route('/', methods = ['GET'])
-@app.route('/index', methods = ['GET'])
+@app.route("/", methods = ["GET"])
+@app.route("/index", methods = ["GET"])
 def redirect_to_root():
-    return redirect('/darkskyweather/api/v1.0/', code = 302)
+    return redirect("/darkskyweather/api/v1.0/", code = 302)
 
-@app.route('/about', methods = ['GET'])
+@app.route("/about", methods = ["GET"])
 def redirect_to_about():
-    return redirect('/darkskyweather/api/v1.0/about', code = 302)
+    return redirect("/darkskyweather/api/v1.0/about", code = 302)
 
-# 1. Landing Page/Index Page:
-@app.route('/darkskyweather/api/v1.0/', methods = ['GET'])
+@app.route("/darkskyweather/api/v1.0/", methods = ["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
     
-# 2. About Page
-@app.route('/darkskyweather/api/v1.0/about', methods = ['GET'])
+@app.route("/darkskyweather/api/v1.0/about", methods = ["GET"])
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template("about.html", title = "About")
 
-# 3. Contact Page
-@app.route('/darkskyweather/api/v1.0/contact', methods = ['GET'])
+@app.route("/darkskyweather/api/v1.0/contact", methods = ["GET"])
 def contact():
-    return render_template('contact.html', title = 'Contact')
+    return render_template("contact.html", title = "Contact")
+    
+@app.route("/darkskyweather/api/v1.0/vancouver", methods = ["GET"])
+def weather_data_yvr():
+    return render_template("weather_data_yvr.html")
+    
+@app.route("/darkskyweather/api/v1.0/vancouver/current", methods = ["GET"])
+def current_weather_yvr():
+    return "Current Weather - Vancouver"
+    
+@app.route("/darkskyweather/api/v1.0/vancouver/alerts", methods = ["GET"])
+def weather_alerts_yvr():
+    return "Weather Alerts - Vancouver"
+    
+@app.route("/darkskyweather/api/v1.0/vancouver/forecast", methods = ["GET"])
+def weather_forecast_yvr():
+    return "Weather Forecast - Vancouver"
+    
+@app.route("/darkskyweather/api/v1.0/vancouver/hindcast", methods = ["GET"])
+def weather_hindcast_yvr():
+    return "Weather Hindcast - Vancouver"
 
 
 
 
 
 # > Weather Data:
-@app.route('/alerts', methods = ['GET'])
+@app.route("/alerts", methods = ["GET"])
 def redirect_to_alerts():
-    return redirect('/darkskyweather/api/v1.0/forecast/alerts/')
+    return redirect("/darkskyweather/api/v1.0/forecast/alerts/")
 
-@app.route('/forecast', methods = ['GET'])
+@app.route("/forecast", methods = ["GET"])
 def redirect_to_forecast():
-    return redirect('/darkskyweather/api/v1.0/forecast/', code = 302)
+    return redirect("/darkskyweather/api/v1.0/forecast/", code = 302)
 
-@app.route('/darkskyweather/api/v1.0/forecast/', methods = ['GET'])
-@app.route('/darkskyweather/api/v1.0/forecast/index', methods = ['GET'])
+@app.route("/darkskyweather/api/v1.0/forecast/", methods = ["GET"])
+@app.route("/darkskyweather/api/v1.0/forecast/index", methods = ["GET"])
 def get_index():
-    forecast = api.getForecastDataFromDarkSky('Vancouver Canada')
+    forecast = api.getForecastDataFromDarkSky("Vancouver Canada")
     # kwkw - returns a python dict
     data = forecast.json()
     return jsonify(data)
 
-@app.route('/darkskyweather/api/v1.0/forecast/alerts/', methods = ['GET'])
+@app.route("/darkskyweather/api/v1.0/forecast/alerts/", methods = ["GET"])
 def get_forecast_alerts():
     # kwkw - Need to format string output
-    alert = dsky.getForecastWeatherAlerts('Vancouver Canada')
+    alert = dsky.getForecastWeatherAlerts("Vancouver Canada")
     return alert
 
-@app.route('/darkskyweather/api/v1.0/forecast/daily/', methods = ['GET'])
+@app.route("/darkskyweather/api/v1.0/forecast/daily/", methods = ["GET"])
 def get_daily_data():
     # kwkw - WIP - will have to refactor once full MVP is constructed.
-    forecast_response = api.getForecastDataFromDarkSky('Vancouver Canada')
+    forecast_response = api.getForecastDataFromDarkSky("Vancouver Canada")
     daily_forecast = dsky.getDailyForecastData(forecast_response.json())
     return jsonify(daily_forecast)
 
-@app.route('/darkskyweather/api/v1.0/forecast/hourly/', methods = ['GET'])
+@app.route("/darkskyweather/api/v1.0/forecast/hourly/", methods = ["GET"])
 def get_hourly_data():
     # kwkw - WIP - will have to refadtor once MVP website is built up.
-    forecast_response = api.getForecastDataFromDarkSky('Vancouver Canada')
+    forecast_response = api.getForecastDataFromDarkSky("Vancouver Canada")
     # kwkw - WIP - hourly data returns an array of
     hourly_forecast = dsky.getHourlyForecastData(forecast_response.json())
     return jsonify(hourly_forecast)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port = 5000, debug = True)
